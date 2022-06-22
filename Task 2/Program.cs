acf01d384e942914732dd126c50d355a51cd7351
belevgen <belsky_evgeniy@mail.rum>
2022-06-22 23:52:09 +0600

    ДЗ Семинар 4_16.06.22_Задача 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число: ");
+string istring = Console.ReadLine();
+int inum = int.Parse(istring);
+int lenght = istring.Lenght;
+int[] arr = new int[lenght];
+int index = 0;
+int sum = 0;
+for (index = 0; index < lenght; index++)
+{   
+    var tmp = inum % 10;
+    inum = inum / 10;
+    sum = sum + tmp;
+}
+Console.Write($"Сумма цифр числа {istring} равна {sum}");
